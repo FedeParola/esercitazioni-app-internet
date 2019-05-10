c407460fec6b2a5a2496df37cfa4c4e03d99f12a
unknown <s255183@studenti.polito.it>
2019-05-10 09:30:08 +0200

    aggiunto bene

---
+++ esercitazione3/src/main/java/it/polito/ai/esercitazione3/security/jwt/InvalidJwtAuthenticationException.java
@@ -0,0 +1,9 @@
+package it.polito.ai.esercitazione3.security.jwt;
+
+import org.springframework.security.core.AuthenticationException;
+
+public class InvalidJwtAuthenticationException extends AuthenticationException {
+    public InvalidJwtAuthenticationException(String e) {
+        super(e);
+    }
+}
---
+++ esercitazione3/src/main/java/it/polito/ai/esercitazione3/services/MailService.java
@@ -0,0 +1,8 @@
+package it.polito.ai.esercitazione3.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class MailService {
+}
