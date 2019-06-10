e390de4426d88d6da36dfc3b5dd975bea1afd48f
unknown <s255183@studenti.polito.it>
2019-06-10 11:16:12 +0200

    aggiunto bene

--- esercitazione5/src/app/app.module.ts
+++ esercitazione5/src/app/app.module.ts
@@ -1,18 +1,43 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MatCardModule} from '@angular/material/card';
+import {MatListModule} from '@angular/material/list';
+import {MatPaginatorModule} from '@angular/material/paginator';
+import {MatChipsModule} from '@angular/material/chips';
+import {MatTabsModule} from '@angular/material/tabs';
+import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login.component';
+import { RegisterComponent } from './register.component';
+import { AttendanceComponent } from './attendance.component';
+import { AttendanceService } from './attendance.service';
+
+
+const routes: Routes = [
+  { path: 'login', component: LoginComponent },
+  { path: 'register', component: RegisterComponent },
+  { path: 'attendance', component: AttendanceComponent }
+];
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent, LoginComponent, RegisterComponent, AttendanceComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatCardModule,
+    MatListModule,
+    MatPaginatorModule,
+    MatChipsModule,
+    MatTabsModule,
+    RouterModule.forRoot(routes, { enableTracing: true })
   ],
-  providers: [],
+  providers: [AttendanceService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
